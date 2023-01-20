de4a308a0da8ee8f8b311ac692a040cc24584eb6
tablangdelio <tablangdelio@gmail.com>
2023-01-20 23:29:05 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+ 
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magasdfic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-nwsahj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-nwsahj)
